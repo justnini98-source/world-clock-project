c0eeb024f791c4ad0837d8565eed56073b4a0276
5-cloud835 <justnini98@hotmail.com>
2026-08-24 07:49:46 +0200

    added current location as an option
    world clock now shows time for the user's current location

--- src/index.js
+++ src/index.js
@@ -35,6 +35,9 @@ aucklandTimeElement.innerHTML = aucklandTime.format("hh:mm:ss [<small>]A[</small
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime= moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
